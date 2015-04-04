448e9371f99b88e8a15d64718bc5e07058e423d7
James Fitzgerald <jtf8647@rit.edu>
2015-04-04 14:16:36 -0400

    Removed a bunch of unused code. Mostly provided login and auth stuff from VS template

--- StocksVC/Global.asax.cs
+++ StocksVC/Global.asax.cs
@@ -22,7 +22,6 @@ namespace StocksVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
